ab6fca81077e105d63fd746c62103a64bffade3a
Dusan Orlovic <dusan.orlovic@trk-inovacije.com>
2014-06-10 17:29:43 +0200

    Adding authentication and a list on index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :check_authentication
+
   def render404
     render :file => File.join(Rails.root, 'public', '404.html'), :status => 404, :layout => false
     return true
+  end
+ 
+  def check_authentication
+    authenticate_user!
   end
 
 end
